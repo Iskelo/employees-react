7adfe2b18f4f30d50c490e2cd4e3fe1a0f2b0b79
Iskelo <61233696+Iskelo@users.noreply.github.com>
2022-11-21 10:49:25 +0100

    Add files via upload

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import "./app-info.css"
+
+
+const AppInfo = () => {
+	return (
+		<div className="app-info">
+			<h1>Employees list company</h1>
+			<h2>Summary employees</h2>
+			<h2>Bonus:</h2>
+		</div>
+	)
+}
+
+export default AppInfo;
